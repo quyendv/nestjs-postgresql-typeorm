499086b84c50eb1f15fb9af612044fb1f0171301
quyendv <daovanquyen45@gmail.com>
2023-08-10 10:45:01 +0700

    feat: find api with condition

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -1,8 +1,8 @@
 import { Profile } from '@modules/profiles/entities/profile.entity';
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { DeepPartial, DeleteResult, Repository } from 'typeorm';
 import { User } from './entities/user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UserService {
@@ -19,7 +19,19 @@ export class UserService {
   }
 
   findAll(): Promise<User[]> {
-    return this.userRepository.find();
+    return this.userRepository.find({
+      loadRelationIds: true, // true nếu lấy ra id của profile trong user (mặc định k có), nếu bật relation thì cái này k quan trọng => Trong db là cột 'profileId' nhưng result lại là 'profile'
+      // where: { id: 1 },
+      // where: { id: In([1, 2, 3, 4, 5]), name: 'Joe Smith', profile: { gender: 'male' } }, // AND
+      where: [
+        // OR each conditionObj
+        { name: 'Joe Smith' },
+        { name: 'Joe Smith1' },
+      ],
+      select: ['id', 'name', 'profile'],
+      // relations: ['profile'],
+      relations: { profile: true },
+    });
   }
 
   findOne(id: number): Promise<User | null> {
