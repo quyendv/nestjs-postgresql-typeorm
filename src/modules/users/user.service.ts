0d3a0eae30fb137104a5a6b3a65d0e433f22b3b5
quyendv <daovanquyen45@gmail.com>
2023-08-10 13:55:57 +0700

    chore: note find options & operators like In, LessThan...

--- src/modules/users/user.service.ts
+++ src/modules/users/user.service.ts
@@ -18,20 +18,28 @@ export class UserService {
     return this.userRepository.save(newUser);
   }
 
+  /**
+   * See more find options: https://typeorm.io/find-options
+   */
   findAll(): Promise<User[]> {
     return this.userRepository.find({
       // loadRelationIds: true, // true nếu lấy ra id của profile trong user (mặc định k có), nếu bật cùng relation thì cái này sẽ conflict và chỉ lấy id => Trong db là cột 'profileId' nhưng result lại là 'profile'
       // where: { id: 1 }, // đổi dạng id sang uuid
-      // where: { id: In([1, 2, 3, 4, 5]), name: 'Joe Smith', profile: { gender: 'male' } }, // AND
+      // where: { id: In([1, 2, 3, 4, 5]), name: 'Joe Smith', profile: { gender: 'male' } }, // AND -> more advanced option like In: https://typeorm.io/find-options#advanced-options
       // where: [
       //   // OR each conditionObj
       //   { name: 'Joe Smith' },
       //   { name: 'Joe Smith1' },
       // ],
-      // select: ['id', 'name', 'profile', 'createdAt', 'updatedAt'],
+      // select: ['id', 'name', 'profile', 'createdAt', 'updatedAt'], // or object<string, boolean> like relations
       // relations: ['profile'], // không nên bật cùng loadRelationIds
       relations: { profile: true },
       // withDeleted: true, // mặc định là false, không lấy những phần tử softDelete/softRemove, bật true nếu vẫn lấy
+
+      // order: { name: 'ASC', id: 'DESC' },
+      // skip: 5, // OFFSET
+      // take: 10, // LIMIT
+      // cache: true, // cache lifetime mặc định 1s, see more: https://typeorm.io/caching#
     });
   }
 
